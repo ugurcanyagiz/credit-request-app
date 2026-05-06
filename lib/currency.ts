466ef04d2fa0388b6a9dbbb77dd93219a85c5b55
ugurcanyagiz <Ugurcanyagiz@gmail.com>
2026-05-06 08:36:09 -0400

    Format credit row currency displays

---
+++ lib/currency.ts
@@ -0,0 +1,10 @@
+const usdCurrencyFormatter = new Intl.NumberFormat("en-US", {
+  style: "currency",
+  currency: "USD",
+  minimumFractionDigits: 2,
+  maximumFractionDigits: 2,
+});
+
+export function formatUsdCurrency(value: number) {
+  return usdCurrencyFormatter.format(value);
+}
