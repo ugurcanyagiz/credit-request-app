99438606a2f541f44caebbd6bf36456f58365267
ugurcanyagiz <Ugurcanyagiz@gmail.com>
2026-05-07 00:26:41 -0400

    Remove dashboard logo

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,7 +1,4 @@
 import type { ReactNode } from "react";
-import Image from "next/image";
-
-import creditAppLogo from "@/components/creditapp.png";
 import { GlobalCartWidget } from "@/components/global-cart-widget";
 
 type DashboardLayoutProps = {
@@ -11,14 +8,6 @@ type DashboardLayoutProps = {
 export default function DashboardLayout({ children }: DashboardLayoutProps) {
   return (
     <div className="min-h-screen">
-      <header className="flex w-full justify-end px-4 pt-4 sm:px-6 sm:pt-6">
-        <Image
-          src={creditAppLogo}
-          alt="Credit App"
-          priority
-          className="h-16 w-auto drop-shadow-sm sm:h-20"
-        />
-      </header>
       <GlobalCartWidget />
       {children}
     </div>
